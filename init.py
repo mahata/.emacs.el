899f4995c0e110ab36df1df474987a33e5305705
mahata <nori@mahata.net>
2012-04-27 08:54:54 -0700

    removed irrelevant comments.

--- init.py
+++ init.py
@@ -1,10 +1,5 @@
 import os
 
-# print "mkdir %s/.emacs.d" % (os.environ['HOME'])
-# print "ln -s %s/.emacs.el %s/.emacs.el" % (os.path.abspath(os.path.dirname(__file__)), os.environ['HOME'])
-# print "ln -s %s/inits %s/.emacs.d/inits" % (os.path.abspath(os.path.dirname(__file__)), os.environ['HOME'])
-# print "ln -s %s/lisp %s/.emacs.d/lisp" % (os.path.abspath(os.path.dirname(__file__)), os.environ['HOME'])
-
 os.system("mkdir %s/.emacs.d" % (os.environ['HOME']))
 os.system("ln -s %s/.emacs.el %s/.emacs.el" % (os.path.abspath(os.path.dirname(__file__)), os.environ['HOME']))
 os.system("ln -s %s/init-loader %s/.emacs.d/init-loader" % (os.path.abspath(os.path.dirname(__file__)), os.environ['HOME']))
