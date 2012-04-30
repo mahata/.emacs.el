2555da07ab8a1bca29d1db3f2d25ca9789a3ce85
mahata <nori@mahata.net>
2012-04-30 02:15:34 -0700

    added a configuration for ruby-flymake.

--- inits/60_flymake.el
+++ inits/60_flymake.el
@@ -50,6 +50,24 @@
          '("\\(.*\\):\\([0-9]+\\):\\(.*\\)" 1 2 nil 3)
          flymake-err-line-patterns)))
 
+(when (not (fboundp 'flymake-ruby-init))
+  (defun flymake-ruby-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
+  (setq flymake-allowed-file-name-masks
+        (append
+         flymake-allowed-file-name-masks
+         '(("\\.rb$" flymake-ruby-init))))
+  (setq flymake-err-line-patterns
+        (cons
+         '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) 
+         flymake-err-line-patterns)))
+
+
 (defun my-popup-flymake-display-error ()
   (interactive)
   (let* ((line-no            (flymake-current-line-no))
