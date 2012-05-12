e066b5a78997c49a4f9206478372fa25b86057fb
mahata <nori@mahata.net>
2012-05-12 12:43:00 -0700

    added /usr/local/bin to eshell's shell-path-env.

--- inits/50_eshell.el
+++ inits/50_eshell.el
@@ -1,3 +1,4 @@
 (add-hook 'eshell-mode-hook
           (defun eshell-mode-hook ()
+            (setq eshell-path-env (concat "/usr/local/bin:" eshell-path-env))
             (define-key eshell-mode-map (kbd "M-s") 'other-window-or-split)))
