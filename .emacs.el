6ef13380c4eea3e174226201a2d9bd049caab83b
mahata <nori@mahata.net>
2012-05-14 23:43:09 -0700

    added haskell-mode.

--- .emacs.el
+++ .emacs.el
@@ -5,6 +5,7 @@
         (expand-file-name "~/.emacs.d/lisp/apel")
         (expand-file-name "~/.emacs.d/lisp/emu")
         (expand-file-name "~/.emacs.d/lisp/vline")
+        (expand-file-name "~/.emacs.d/lisp/haskell-mode")
         (expand-file-name "~/.emacs.d/lisp/anything")
         (expand-file-name "~/.emacs.d/lisp/auto-complete")
         (expand-file-name "~/.emacs.d/lisp/auto-install")
