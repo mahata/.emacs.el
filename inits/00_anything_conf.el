74045e19ac88a6eefdf8bb543c3b2a3eaf3321be
mahata <nori@mahata.net>
2012-05-15 23:07:29 -0700

    modified anything related configuration parameters.

--- inits/00_anything_conf.el
+++ inits/00_anything_conf.el
@@ -1,2 +1,6 @@
 (eval-after-load "anything"
   '(define-key anything-map (kbd "C-h") 'delete-backward-char))
+
+(setq anything-candidate-number-limit 10)
+(setq anything-input-idle-delay 0.01)
+(setq anything-idle-delay 0.01)
