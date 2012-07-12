8f14daf73f21ac4c8d5b532915e9ba0a93ff3b30
Yasunori Mahata <nori@mahata.net>
2012-07-12 10:08:56 -0700

    added a quickrun setting for clojure.

--- inits/10_quickrun_conf.el
+++ inits/10_quickrun_conf.el
@@ -6,3 +6,9 @@
                       '((:command . "rhino")))
 (add-to-list 'quickrun-file-alist
              '("\\.js$" . "rhino"))
+
+;; clojure
+(quickrun-add-command "clojure"
+                      '((:command . "clj")))
+(add-to-list 'quickrun-file-alist
+             '("\\.clj$" . "clojure"))
