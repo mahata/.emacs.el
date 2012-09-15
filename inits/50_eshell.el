19d11d73715cc19431ef84096d3cb70658212467
mahata <nori@mahata.net>
2012-09-14 23:13:07 -0700

    modified some hooks.

--- inits/50_eshell.el
+++ inits/50_eshell.el
@@ -1,4 +1,5 @@
-(add-hook 'eshell-mode-hook
-          (defun eshell-mode-hook ()
-            (setq eshell-path-env (concat "/usr/local/bin:" eshell-path-env))
-            (define-key eshell-mode-map (kbd "M-s") 'other-window-or-split)))
+(defun eshell-mode-hook-func ()
+  (setq eshell-path-env (concat "/usr/local/bin:" eshell-path-env))
+  (define-key eshell-mode-map (kbd "M-s") 'other-window-or-split))
+
+(add-hook 'eshell-mode-hook 'eshell-mode-hook-func)
--- inits/50_javascript_conf.el
+++ inits/50_javascript_conf.el
@@ -1,6 +1,6 @@
-(add-hook 'javascript-mode-hook
-          (defun javascript-mode-hook ()
-            (setq-default indent-tabs-mode nil)
-            (setq-default indent-level 4)
-            (setq-default tab-width 4)
-            ))
+(defun js-mode-hook-func ()
+  (setq-default indent-tabs-mode nil)
+  (setq-default indent-level 4)
+  (setq-default tab-width 4))
+
+(add-hook 'js-mode-hook 'js-mode-hook-func)
