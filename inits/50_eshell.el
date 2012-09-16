a971be905f41d901862ef5dd8166662b17553826
mahata <nori@mahata.net>
2012-09-16 10:35:52 -0700

    added a path to PATH env, in addition to eshell-path-env.

--- inits/50_eshell.el
+++ inits/50_eshell.el
@@ -1,5 +1,6 @@
 (defun eshell-mode-hook-func ()
   (setq eshell-path-env (concat "/usr/local/bin:" eshell-path-env))
+  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
   (define-key eshell-mode-map (kbd "M-s") 'other-window-or-split))
 
 (add-hook 'eshell-mode-hook 'eshell-mode-hook-func)
