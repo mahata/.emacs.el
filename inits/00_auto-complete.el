5919e8edc6142005eb6d21b752495aae0cd6e007
mahata <nori@mahata.net>
2013-06-03 01:24:30 -0700

    deleted some more packages.

---
+++ inits/00_auto-complete.el
@@ -0,0 +1,7 @@
+(require 'auto-complete-config)
+(global-auto-complete-mode 1)
+(ac-config-default)
+(setq ac-menu-height 20)
+(setq ac-auto-start 2)
+(setq ac-use-menu-map t)
+
--- inits/10_quickrun_conf.el
+++ inits/10_quickrun_conf.el
@@ -1,5 +1,5 @@
-(require 'quickrun)
-(push '("*quickrun*") popwin:special-display-config)
+;; (require 'quickrun)
+;; (push '("*quickrun*") popwin:special-display-config)
 
 ;; javascript
 
@@ -8,14 +8,14 @@
 ;; (add-to-list 'quickrun-file-alist
 ;;              '("\\.js$" . "rhino"))
 
-(quickrun-add-command "node"
-                      '((:command . "~/.nodebrew/current/bin/node")
-                        (:exec . "%c %s")))
-(add-to-list 'quickrun-file-alist
-             '("\\.js$" . "node"))
+;; (quickrun-add-command "node"
+;;                       '((:command . "~/.nodebrew/current/bin/node")
+;;                         (:exec . "%c %s")))
+;; (add-to-list 'quickrun-file-alist
+;;              '("\\.js$" . "node"))
 
 ;; clojure
-(quickrun-add-command "clojure"
-                      '((:command . "clj")))
-(add-to-list 'quickrun-file-alist
-             '("\\.clj$" . "clojure"))
+;; (quickrun-add-command "clojure"
+;;                       '((:command . "clj")))
+;; (add-to-list 'quickrun-file-alist
+;;              '("\\.clj$" . "clojure"))
