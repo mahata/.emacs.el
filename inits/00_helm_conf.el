1aaa45470b7baf89c2386bcb7a849392d210b477
Yasunori Mahata <nori@mahata.net>
2013-07-15 09:57:16 -0700

    modified helm conf.

--- inits/00_helm_conf.el
+++ inits/00_helm_conf.el
@@ -6,3 +6,14 @@
 (setq helm-idle-delay 0.001)
 
 (setq helm-c-filelist-file-name "/var/tmp/all.filelist")
+
+(require 'helm-config)
+(require 'helm-gtags)
+
+(add-hook 'helm-gtags-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
+             (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
+             (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
+             (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)
+             (local-set-key (kbd "C-c C-f") 'helm-gtags-find-files)))
