603e7846968934be7a40824548052396b02d038f
Yasunori Mahata <nori@mahata.net>
2013-07-17 15:39:05 -0700

    modified to use helm-gtags-mode automatically for php files.

--- inits/50_php-mode_conf.el
+++ inits/50_php-mode_conf.el
@@ -4,6 +4,7 @@
   (if (eq (getenv "USER") "mahata")
       (setq indent-tabs-mode nil)
       (setq indent-tabs-mode t))
+  (helm-gtags-mode t)
   (setq indent-level 4)
   (setq c-basic-offset 4)
   (setq tab-width 4))
