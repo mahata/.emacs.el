74c952a7a90b0fe9093b379b6fa62c60fa260a5a
Yasunori Mahata <nori@mahata.net>
2013-07-29 11:19:12 -0700

    modified to use helm-gtags-mode for javascript-mode as well.

--- inits/50_javascript_conf.el
+++ inits/50_javascript_conf.el
@@ -2,6 +2,7 @@
   (if (eq (getenv "USER") "mahata")
       (setq indent-tabs-mode nil)
       (setq indent-tabs-mode t))
+  (helm-gtags-mode t)
   (setq-default indent-level 4)
   (setq-default tab-width 4))
 
