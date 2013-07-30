e780167e54488cdac972521fc620025cce1746f8
Yasunori Mahata <nori@mahata.net>
2013-07-30 15:13:42 -0700

    modified to configure yasnippet.

--- inits/60_yasnippet.el
+++ inits/60_yasnippet.el
@@ -1,2 +1,11 @@
 (require 'yasnippet)
+
+(add-to-list 'yas-snippet-dirs "~/emacs-conf/yasnippet")
+
+(require 'dropdown-list)
+(setq yas/prompt-functions '(yas/dropdown-prompt
+                             yas/ido-prompt
+                             yas/completing-prompt))
+
 (yas-global-mode 1)
+
