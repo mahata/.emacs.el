b0f457c548bbf600f836c12c474299a96dd6c4b3
Yasunori Mahata <yasunori.mahata@hootsuite.com>
2013-08-17 14:25:56 -0700

    switched to js3 mode.

--- inits/50_javascript_conf.el
+++
@@ -1,9 +0,0 @@
-(defun js-mode-hook-func ()
-  (if (eq (getenv "USER") "mahata")
-      (setq indent-tabs-mode nil)
-      (setq indent-tabs-mode t))
-  (helm-gtags-mode t)
-  (setq-default indent-level 4)
-  (setq-default tab-width 4))
-
-(add-hook 'js-mode-hook 'js-mode-hook-func)
---
+++ inits/50_js3_conf.el
@@ -0,0 +1,17 @@
+(setq js3-mirror-mode t)
+(autoload 'js3-mode "js3" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
+
+(defun js3-mode-hooks ()
+  (setq js3-indent-level 4)
+  (setq js3-mode-dev-mode-p t)
+  (setq js3-auto-indent-p t)
+  (setq js3-enter-indents-newline t)
+  (setq js3-indent-on-enter-key t)
+  (when (require 'auto-complete nil t)
+    (make-variable-buffer-local 'ac-sources)
+    (add-to-list 'ac-sources 'ac-source-yasnippet)
+    (auto-complete-mode t)))
+
+(add-hook 'js3-mode-hook 'js3-mode-hooks)
+
