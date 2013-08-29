86de0c59f5668a972b179463feb245fe2548880c
Yasunori Mahata <nori@mahata.net>
2013-08-29 12:58:05 -0700

    modified to use hs-minor-mode even for java-mode.

--- inits/50_java_conf.el
+++ inits/50_java_conf.el
@@ -1,6 +1,7 @@
-(add-hook 'java-mode-hook
-          (defun java-mode-hook ()
-            (setq-default indent-tabs-mode nil)
-            (setq-default indent-level 4)
-            (setq-default tab-width 4)
-            ))
+(defun java-mode-hooks()
+  (setq-default indent-tabs-mode nil)
+  (setq-default indent-level 4)
+  (setq-default tab-width 4)
+  (hs-minor-mode 1))
+
+(add-hook 'java-mode-hook 'java-mode-hooks)
