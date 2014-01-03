edaadea023a3ec24cc25a3c6bb860d80c151e0be
Yasunori Mahata <nori@mahata.net>
2014-01-03 01:53:31 -0800

    added helm-ls-git and added a keybind for it.

--- inits/90_keybindings.el
+++ inits/90_keybindings.el
@@ -22,6 +22,7 @@
 (define-key global-map (kbd "C-S-u") 'undo)
 (define-key global-map (kbd "M->") 'pop-tag-mark)
 (define-key global-map (kbd "C-c g") 'helm-git-grep)
+(define-key global-map (kbd "C-c h") 'helm-ls-git-ls)
 
 (define-key c++-mode-map "\C-c:" 'uncomment-region)
 ;; (define-key dired-mode-map "\M-s" 'other-window-or-split)
